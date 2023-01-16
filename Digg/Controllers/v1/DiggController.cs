2620e446bef6af5bf47aa2747b23c33a3fe38c5b
Johan Karlsson <johan.b.karlsson@slu.se>
2023-01-16 15:21:20 +0100

    API Message.

--- Digg/Controllers/v1/DiggController.cs
+++ Digg/Controllers/v1/DiggController.cs
@@ -1,4 +1,6 @@
-﻿namespace Digg.Controllers.v1
+﻿using Digg.Models;
+
+namespace Digg.Controllers.v1
 {
     [ApiVersion("1.0")]
     public class DiggController : ApiControllerBase
@@ -9,9 +11,17 @@
         [SwaggerResponse(StatusCodes.Status409Conflict)]
         [SwaggerResponse(StatusCodes.Status500InternalServerError)]
         [SwaggerResponse(StatusCodes.Status503ServiceUnavailable)]
-        public string Index()
-        {           
-            return $"Hello v1.0! Current time: {DateTime.UtcNow.ToString("o")}";
+        public CamelCaseExample Index(int id)
+        {
+            var utcNow = DateTime.UtcNow;
+
+            return new CamelCaseExample()
+            {
+                IdExample = id,
+                NameExample = "camelCase!",
+                DateTimeExample = utcNow,
+                DateTimeStringExample = utcNow.ToString("o")
+            };
         }
     }
 }
---
+++ Digg/Models/CamelCaseExample.cs
@@ -0,0 +1,10 @@
+﻿namespace Digg.Models
+{
+    public class CamelCaseExample
+    {
+        public int IdExample { get; set; }
+        public string? NameExample { get; set; }
+        public DateTime DateTimeExample { get; set; }
+        public string? DateTimeStringExample { get; set; }
+    }
+}
